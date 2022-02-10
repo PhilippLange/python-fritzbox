a118695cac9de6174a8b5045c16dbd6103c8ea31
Philipp Lange <philipp.i.b.lange@gmail.com>
2022-02-10 17:43:24 +0100

    add requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ import os
 from setuptools import setup
 
 setup(name='python-fritzbox',
-  version='0.2',
+  version='0.3',
   description='Automate the Fritz!Box with python',
-  url='https://github.com/pamapa/python-fritzbox',
-  author='Patrick Ammann',
-  author_email='pammann@gmx.net',
+  url='https://github.com/PhilippLange/python-fritzbox',
+  author='Philipp Lange',
+  author_email='philipp.i.b.lange@gmail.com',
   license='GNU',
   packages=['fritzbox', 'tools'],
   zip_safe=False,
+  install_requires=[
+      'Pillow',
+      'ldif3',
+      'requests',
+      'vobject'
+  ],
   scripts=[
     os.path.join('tools', 'fritzboxphonebook.py'),
     os.path.join('tools', 'fritzboxktipp.py'),
